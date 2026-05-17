49bee1fd6467e96ec251bf2f847e3adf56a590e0
Ayush-2524 <shineassun1121@gmail.com>
2026-05-17 11:36:22 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,153 @@
+import './App.css';
+import Alert from './components/Alert';
+// import About from './components/About';
+import Navbar from './components/Navbar';
+import TextForm from './components/TextForm';
+import React, { useState } from 'react'
+// import {
+//   BrowserRouter as Router,
+//   Routes,
+//   Route
+// } from "react-router-dom";
+
+function App() {
+   const [mode, setMode] = useState('light');//whether darkmode is enabled or not
+   const [alert, setAlert] = useState(null);
+
+   const showAlert=(message,type)=>{
+    setAlert({
+      msg: message,
+      type: type
+    })
+    setTimeout(() => {
+      setAlert(null);
+    }, 1500);
+   }
+  const toggleMode = () => {
+    if (mode === 'light') {
+      setMode('dark');
+      document.body.style.backgroundColor = '#042743';
+      showAlert("Dark mode has been enabled", "success");
+      // document.title = 'TextUtils - Dark Mode';
+      // setInterval(() => {
+      //   document.title = 'TextUtils is Amazing Mode';
+      // }, 2000);
+      // setInterval(() => {
+      //   document.title = 'Install TextUtils Now';
+      // }, 1500);
+    }
+    else {
+      setMode('light');
+      document.body.style.backgroundColor = 'white';
+      showAlert("Light mode has been enabled", "success"); 
+      document.title = 'TextUtils - Light Mode';
+    }
+  }
+ 
+  return (
+    <>
+    <Navbar title="TextUtils" abouttext="About TextUtils" mode={mode}  toggleMode={toggleMode}/>
+      <Alert alert={alert} />
+    <div className="container my-3">
+          {/* <About />  */}
+            <TextForm showAlert={showAlert} heading="Enter the text to analyze below" mode={mode}/>
+    </div>
+    </>
+  );
+}
+
+export default App;
+
+// import './App.css';
+// import React, { useState } from 'react';
+
+// import Navbar from './components/Navbar';
+// import TextForm from './components/TextForm';
+// import About from './components/About';
+// import Alert from './components/Alert';
+
+// import {
+//   BrowserRouter as Router,
+//   Routes,
+//   Route
+// } from "react-router-dom";
+
+// function App() {
+
+//   const [mode, setMode] = useState('light');
+//   const [alert, setAlert] = useState(null);
+
+//   const showAlert = (message, type) => {
+
+//     setAlert({
+//       msg: message,
+//       type: type
+//     });
+
+//     setTimeout(() => {
+//       setAlert(null);
+//     }, 1500);
+//   }
+
+//   const toggleMode = () => {
+
+//     if (mode === 'light') {
+
+//       setMode('dark');
+
+//       document.body.style.backgroundColor = '#042743';
+
+//       showAlert("Dark mode has been enabled", "success");
+
+//     } else {
+
+//       setMode('light');
+
+//       document.body.style.backgroundColor = 'white';
+
+//       showAlert("Light mode has been enabled", "success");
+//     }
+//   }
+
+//   return (
+
+//     <Router>
+
+//       <Navbar
+//         title="TextUtils"
+//         abouttext="About"
+//         mode={mode}
+//         toggleMode={toggleMode}
+//       />
+
+//       <Alert alert={alert} />
+
+//       <div className="container my-3">
+
+//         <Routes>
+
+//           <Route
+//             path="/about"
+//             element={<About />}
+//           />
+
+//           <Route
+//             path="/"
+//             element={
+//               <TextForm
+//                 showAlert={showAlert}
+//                 heading="Enter the text to analyze below"
+//                 mode={mode}
+//               />
+//             }
+//           />
+
+//         </Routes>
+
+//       </div>
+
+//     </Router>
+//   );
+// }
+
+// export default App;
---
+++ src/components/TextForm.js
@@ -0,0 +1,46 @@
+import React,{useState} from 'react'
+
+
+export default function TextForm(props) {
+    const handleClick=()=>{
+        let newText=text.toUpperCase();
+        setText(newText);
+        props.showAlert("Converted to Uppercase!","success");
+    }
+     const handleLowClick=()=>{
+        let newText=text.toLowerCase();
+        setText(newText);
+        props.showAlert("Converted to Lowercase!","success");
+    }
+    const handleOnChange=(event)=>{
+        // setText(event.target.value);
+        console.log("On Change");
+        setText(event.target.value);
+        props.showAlert("Text Changed!","success");
+    }
+    const [text, setText] = useState('');
+    return (
+        <>
+    <div className="container" style={{color:props.mode==='dark'?'white':'#042743'}}>
+        <h1>{props.heading}</h1>
+        <div className="mb-3">
+            <textarea className="form-control" value={text} onChange={handleOnChange} style={{backgroundColor:props.mode==='dark'?'grey':'light' , color: props.mode==='dark'?'white':'#042743'}} id="myBox" rows="8"></textarea>
+        </div>
+        <button className="btn btn-primary" onClick={handleClick}>Convert to Uppercase</button>
+        <button className="btn btn-primary mx-2" onClick={handleLowClick}>Convert to Lowercase</button>
+        <button className="btn btn-primary mx-2" onClick={() => setText('')}>Clear Text</button>
+        <button className="btn btn-primary mx-2" onClick={() => {navigator.clipboard.writeText(text); props.showAlert("Text copied to clipboard!", "success")}}>Copy Text</button>
+        <button className="btn btn-primary mx-2" onClick={() => {setText(text.split(' ').filter((element)=>{return element.length!==0}).join(" ")); props.showAlert("Extra spaces removed!", "success")}}>Remove Extra Spaces</button>
+        <button className="btn btn-primary mx-2" onClick={() => {setText(text.split(' ').reverse().join(" ")); props.showAlert("Words reversed!", "success")}}>Reverse Words</button>
+    </div>
+    <div className="conatainer my-3" style={{color:props.mode==='dark'?'white':'#042743'}}>
+        <h2>Your text summary</h2>
+        <p>{text.split(" ").length} words and {text.length} characters</p>
+        <p>{0.008*text.split(" ").length} Minutes read</p>
+        <h2>Preview</h2>
+        <p>{text.length>0?text:"Enter something in the textbox above to preview it here"}</p>
+    </div>
+    </>
+  )
+}
+
